a545c335219115fb3d14133db3456e68cdf0814a
SH-NicholasPage <npage@southhills.edu>
2022-11-28 23:08:55 -0500

    Initial commit

---
+++ IT100-Homework1/IT100-Homework1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IT100_Homework1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            String wizardOfOz = "Toto, I've a feeling we're not in Kansas anymore.";
+            //TODO: Your code here
+        }
+    }
+}
