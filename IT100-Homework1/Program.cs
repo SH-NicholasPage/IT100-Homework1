881f67fe830505c6b9a98a7579c999abe5b1eacb
Nicholas Page <npage@southhills.edu>
2024-11-27 16:07:24 -0500

    Restructure

---
+++ IT100-Homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+namespace IT100_Homework1;
+
+public class Program
+{
+    public static void Main()
+    {
+        String wizardOfOz = "Toto, I've a feeling we're not in Kansas anymore.";
+        //TODO: Your code here
+    }
+}
