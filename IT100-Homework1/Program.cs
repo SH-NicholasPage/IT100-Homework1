398bd9bec05306c19693a1aa63f38ca6a0dbcb9b
Nicholas Page <npage@southhills.edu>
2024-12-17 10:33:43 -0500

    Added sections for the code

--- IT100-Homework1/Program.cs
+++ IT100-Homework1/Program.cs
@@ -10,6 +10,18 @@ public class Program
     public static void Main()
     {
         String wizardOfOz = "Toto, I've a feeling we're not in Kansas anymore.";
-        //TODO: Your code here
+        // TODO: Part 1
+        
+        // TODO: Part 2
+        
+        // TODO: Part 3
+        
+        // TODO: Part 4
+        
+        // TODO: Part 5
+        
+        // TODO: Part 6
+        
+        // TODO: Part 7
     }
 }
